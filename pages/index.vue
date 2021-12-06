4070682bbc8df4e2ee8a8c2f7eff525a369d2996
DESKTOP-EVSDQ7Q\Wu <shapl@qq.com>
2021-12-07 00:11:25 +0800

    init layout

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,28 @@
 <template>
-  <Tutorial/>
+  <h1>{{ title }}</h1>
 </template>
 
 <script>
-export default {}
+export default {
+    head() {
+        return {
+            title: '在线引流',
+            meta: [
+                {
+                    name: 'description',
+                    content: 'My custom description'
+                },
+                {
+                    name: 'keyword',
+                    content: 'My custom keyword'
+                }
+            ]
+        }
+    },
+    data() {
+        return {
+            title: 'Hello World!'
+        }
+    },
+}
 </script>
